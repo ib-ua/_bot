5a1cfebbcd71712987c6b2a331c768a3e49e4c63
Ievgen Bardadym <ievgen.bardadym@xtendx>
2023-12-18 01:37:46 +0100

    feat(package): Make application installable package, provide basic application structure

---
+++ bot/Bot.py
@@ -0,0 +1,16 @@
+from bot.controller.InputProcessor import InputProcessor
+from bot.models.AddressBook import AddressBook
+
+
+class Bot:
+    def __init__(self,  address_book_name):
+        self.address_book = AddressBook(address_book_name)
+        self.processor = InputProcessor(self.address_book)
+
+    def start(self) -> None:
+        while self.address_book.is_open:
+            print(
+                self.processor.process(
+                    input("What would you like to do?\n")
+                )
+            )
