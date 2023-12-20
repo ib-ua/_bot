39d38b4cb27778e241fac7a62bafbacebedd43df
Ievgen Bardadym <ievgen.bardadym@xtendx>
2023-12-20 19:46:31 +0100

    feat(address_book): Provide examples of commands

--- src/bot/controller/input_processor.py
+++ src/bot/controller/input_processor.py
@@ -1,7 +1,10 @@
 import re
 from collections import UserDict
+from typing import List
+
 from ..models import AddressBook
 from ..models import NoteBook
+from ..models.record import Record
 
 
 class InputProcessor(UserDict):
@@ -9,8 +12,40 @@ class InputProcessor(UserDict):
         super().__init__()
         self.address_book = address_book
         self.note_book = note_book
-        self.data['exit'] = lambda args: self.address_book.close()
+        self.context = None
+
+        self.data[None] = {
+            'exit': lambda args: self.address_book.close(),
+            'hello': lambda args: 'Hello',
+            'add-contact': lambda args: self.create_contact(*args)
+        }
+
+        self.data[Record] = {
+            'add-phones': lambda args: self.add_phone(*args),
+            'cancel': lambda args: self.cancel_work_on_record(),
+            'ok': lambda args: self.complete_work_on_record()
+        }
+
+    def get_input_message(self) -> List[str]:
+        d: dict = self.data.get(type(self.context), self.data[None])
+        return list(d.keys())
 
     def process(self, user_input: str) -> str:
         [command, *args] = re.split(r'\s+', user_input.strip())
-        return self.data.get(command, lambda x: "Command not found")(args)
+        return self.data.get(type(self.context), self[None]).get(command, lambda x: "Command not found")(args)
+
+    def create_contact(self, name: str):
+        self.context = Record(name)
+
+    def add_phone(self, phone: str):
+        record = self.context
+        record.add_phone(phone)
+
+    def complete_work_on_record(self):
+        self.address_book.add_record(self.context)
+        self.context = None
+        return 'Contact saved successfully'
+
+    def cancel_work_on_record(self):
+        self.context = None
+        return 'Creating contact canceled'
